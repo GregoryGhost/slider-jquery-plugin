c997468ce2a5345c825513a02c2035cdf3db02e5
Gregory <lobster287@gmail.com>
2018-09-13 03:05:26 +0700

    Added sample using jest with TS

---
+++ src/Calculator.ts
@@ -0,0 +1,16 @@
+export default class calculator {
+  static Sum(a: number, b: number): number {
+    let c = a + b;
+    return c;
+  }
+
+  static Difference(a: number, b: number): number {
+    let c = a - b;
+    return c;
+  }
+
+  static Mul(a: number, b: number): number {
+    let c = a * b;
+    return c;
+  }
+}

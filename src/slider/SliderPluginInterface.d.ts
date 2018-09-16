60ced40dcf7bc80847f9c62613b063dd6dad51fe
Gregory <lobster287@gmail.com>
2018-09-16 08:47:47 +0700

    Added draft version interfaces for Slider Plugin and Slider Service

--- src/slider/SliderPluginInterface.d.ts
+++ src/slider/SliderPluginInterface.d.ts
@@ -1,44 +1,60 @@
-/**
- * Options for the example plugin.
- */
-interface ExamplePluginOptions {
-    /**
-    * CSS selector for the element where generated messages are inserted. (required)
-    */
-    outputSelector: string;
-    /**
-    * Color of the message text. (optional)
-    */
-    outputColor?: string;
+type Orientation =
+    | 'vertical'
+    | 'horizontal';
+
+interface BasicOptions {
+    min?: number;
+    max?: number;
+    value?: number;
+    step?: number;
+    orientation?: Orientation;
+}
+
+type Position =
+    | 'handle'
+    | 'handle_over'
+    | 'handle_under';
+
+interface FloatOptions {
+    position?: Position;
+}
+
+type HideLabels = false
+
+type LabelsOfPips =
+    | string[]
+    | Endpoints
+    | HideLabels;
+
+interface PipsOptions {
+    labels?: LabelsOfPips
 }
 
 /**
- * Global options of the example plugin available as properties on $.fn object.
+ * Options for the example plugin.
  */
-interface ExamplePluginGlobalOptions {
-    /**
-    * Global options of the example plugin.
-    */
-    options: ExamplePluginOptions;
-}
+type SliderPluginOptions =
+    | BasicOptions
+    | PipsOptions
+    | FloatOptions;
 
 /**
  * Function to apply the example plugin to the selected elements of a jQuery result.
  */
-interface ExamplePluginFunction {
+interface SliderPluginFunction {
     /**
     * Apply the example plugin to the elements selected in the jQuery result.
     *
     * @param options Options to use for this application of the example plugin.
     * @returns jQuery result.
     */
-    (options: ExamplePluginOptions): JQuery;
+    (options?: SliderPluginOptions): JQuery;
 }
 
 /**
  * Declaration of the example plugin.
  */
-interface ExamplePlugin extends ExamplePluginGlobalOptions, ExamplePluginFunction { }
+interface SliderPlugin extends SliderPluginFunction { }
 
 /**
  * Extend the jQuery result declaration with the example plugin.
@@ -47,5 +63,5 @@ interface JQuery {
     /**
     * Extension of the example plugin.
     */
-    examplePlugin: ExamplePlugin;
+    sliderPlugin: SliderPlugin;
 }
